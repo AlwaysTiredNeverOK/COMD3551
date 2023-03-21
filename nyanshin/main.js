c8ecb52d7bda7c64549e6e413e4ae2edd4115a68
AlwaysTiredNeverOK <112575070+AlwaysTiredNeverOK@users.noreply.github.com>
2023-03-21 15:05:02 -0400

    Delete nyanshin directory

--- nyanshin/main.js
+++
@@ -1,9 +0,0 @@
-var modal = document.getElementById('wish-result');
-var wishButton = document.getElementbyClass('wish-button');
-var closeModal = document.getElementbyId('close-button');
-
-wishButton.addEventListener('click', openModal);
-
-function openModal(){
-	modal.style.display = 'block';
-}
